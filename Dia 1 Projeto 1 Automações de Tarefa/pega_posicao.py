662d806f924466b95947a014ff209d129ea57c89
wellingt0n-0liveira <lasombrarj@gmail.com>
2026-01-16 20:03:36 -0300

    corrigindo legendas

--- Dia 1 Projeto 1 Automações de Tarefa/pega_posicao.py
+++ Dia 1 Projeto 1 Automações de Tarefa/pega_posicao.py
@@ -1,7 +1,9 @@
+
 from cmath import nan
 import pyautogui
 import time
 
+# Aguarda 5 segundos para o usuário posicionar o mouse na tela
 time.sleep(5)  # Tempo para o usuário posicionar o mouse
 print(pyautogui.position()) # Imprime a posição atual do mouse
 
